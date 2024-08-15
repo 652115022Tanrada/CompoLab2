a5b1a35a1b8144b4dcb93858e2a1a86aaf151ebc
Tanrada <tanrada_wichian@cmu.ac.th>
2024-08-16 02:39:41 +0700

    2.2

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -35,7 +35,7 @@ onMounted(() => {
   <div class="home">
     <h1>Events For Good</h1>
     <!-- new element -->
-    <div class="events">
+    <div class="flex flex-col items-center">
       <EventCard v-for="event in events" :key="event.id" :event="event" />
   <div class="pagination">
     <RouterLink
@@ -57,11 +57,7 @@ onMounted(() => {
 </template>
 
 <style>
-  .events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+
 .pagination {
   display: flex;
   width: 290px;
