eb5c33b3997846816619a497ac133f13614f2cb7
Tanrada <tanrada_wichian@cmu.ac.th>
2024-10-03 23:45:14 +0700

    6.3 Add OrganizerService.ts

--- 331-intro-to-vite/src/views/EventFormView.vue
+++ 331-intro-to-vite/src/views/EventFormView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import type { Event } from '@/types'
-import { ref } from 'vue'
+import type { Event, Organizer } from '@/types'
+import { onMounted, ref } from 'vue'
 import EventService from '@/services/EventService'
+import OrganizerService from '@/services/OrganizerService'
 import { useRouter } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import BaseInput from '@/components/BaseInput.vue'
@@ -36,6 +37,17 @@ function saveEvent() {
       router.push({ name: 'network-error-view' })
     })
 }
+
+const organizers = ref<Organizer[]>([])
+onMounted(() => {
+  OrganizerService.getOrganizers()
+    .then((response) => {
+      organizers.value = response.data
+    })
+    .catch(() =>  {
+      router.push({ name: 'network-error-view'})
+    })
+})
 </script>
 
 <template>
@@ -54,7 +66,7 @@ function saveEvent() {
           </div>
 
           <div class="form-group">
-            <BaseInput v-model="event.description" id="description" type="text" label="Description" /> 
+            <BaseInput v-model="event.description" id="description" type="text" label="Description" />
           </div>
         </div>
 
@@ -65,6 +77,21 @@ function saveEvent() {
           </div>
         </div>
 
+        <div class="form-section">
+          <h3>Who is your organizer?</h3>
+          <div class="form-group">
+            <label>Select an organizer</label>
+            <select v-model="event.organizer.id">
+              <option 
+                v-for="option in organizers"
+                :value="option.id"
+                :key="option.id"
+                :selected="option.id === event.organizer.id"
+              > {{ option.name }}
+              </option>
+            </select>
+          </div>
+        </div>
         <button class="submit-btn" type="submit">Submit</button>
       </form>
     </div>
@@ -160,7 +187,7 @@ h2 {
 }
 
 .submit-btn:hover {
-  background-color: #4caf50;
+  background-color: #45a049;
 }
 
 .pre-container {
@@ -179,39 +206,3 @@ pre {
   word-wrap: break-word;
 }
 </style>
-
-<!-- <script setup lang="ts">
-import type { EventItem } from '@/types';
-import { ref } from 'vue';
-const event = ref<EventItem> ({
-    id: 0,
-    category: '',
-    title: '',
-    description: '',
-    location: '',
-    date: '',
-    time: '',
-    organizer: ''
-})
-</script>
-
-<template>
-    <div>
-        <h1>Create an event</h1>
-        <form>
-            <label>category</label>
-            <input v-model="event.category" type="text" placeholder="Category" class="field">
-            <h3>Name & describe your event</h3>
-            <label>Title</label>
-            <input v-model="event.title" type="text" placeholder="Title" class="field">
-            <label>Description</label>
-            <input v-model="event.description" type="text" placeholder="Description" class="field">
-            <h3>Where is your event?</h3>
-            <label>Location</label>
-            <input v-model="event.location" type="text" placeholder="Location" class="field">
-            <button class="button" type="submit">Submit</button>
-        </form>
-
-        <pre>{{ event }}</pre>
-    </div>
-</template> -->
